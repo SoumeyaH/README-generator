fd9a51cd981802940e94da623f233ef4c8fef477
Soumeya <soumeyahassan47@gmail.com>
2021-05-02 20:26:17 +0100

    inquirer question added

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
-// TODO: Include packages needed for this application
+// // TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// const inquirer = require("inquirer");
 
-// TODO: Create a function to write README file
-const writeToFile = (fileName, data) => {};
+// // TODO: Create an array of questions for user input
+// const questions = [
+//   {
+//     type = "input",
+//     message = "What is your name?",
+//     name = "yourName"
+//   }
+// ];
 
-// TODO: Create a function to initialize app
-const init = () => {};
+// // TODO: Create a function to write README file
+// const writeToFile = (fileName, data) => {};
+
+// // TODO: Create a function to initialize app
+// const init = () => {
+//   inquirer.prompt
+// };
 
 // Function call to initialize app
+
+const inquirer = require("inquirer");
+// function to handle your answers
+const handleAnswers = (answers) => {
+  console.log(answers);
+};
+// generic function to get answers for questions
+const getAnswersFromQuestions = async (questions) => {
+  const answers = await inquirer.prompt(questions);
+  return answers;
+};
+const init = async () => {
+  const questions = [
+    {
+      type: "input",
+      message: "What is your user name?",
+      name: "username",
+    },
+  ];
+  const answers = await getAnswersFromQuestions(questions);
+  handleAnswers(answers);
+};
 init();
